62fb7e7bcf766fb34e9bd1484d8759c8d86f940c
mousaamiri <106370749+mousaamiri@users.noreply.github.com>
2026-07-02 20:59:31 +0330

    feat: add AppDbContext with DbSets for all entities and translations

---
+++ src/Infrastructure/Portfolio.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Portfolio.Domain.Entities.Educations;
+using Portfolio.Domain.Entities.Experiences;
+using Portfolio.Domain.Entities.Projects;
+using Portfolio.Domain.Entities.Skills;
+
+namespace Portfolio.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Education> Educations => Set<Education>();
+    public DbSet<EducationTranslation> EducationTranslations => Set<EducationTranslation>();
+
+    public DbSet<Experience> Experiences => Set<Experience>();
+    public DbSet<ExperienceTranslation> ExperienceTranslations => Set<ExperienceTranslation>();
+
+    public DbSet<Project> Projects => Set<Project>();
+    public DbSet<ProjectTranslation> ProjectTranslations => Set<ProjectTranslation>();
+
+    public DbSet<Skill> Skills => Set<Skill>();
+    public DbSet<SkillTranslation> SkillTranslations => Set<SkillTranslation>();
+}
