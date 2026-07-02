e7a0157bdea5db017f4219a6f2e65692ad074b2c
mousaamiri <106370749+mousaamiri@users.noreply.github.com>
2026-07-02 19:54:52 +0330

    feat: add generic IRepository<T> interface in Application layer

---
+++ src/Core/Portfolio.Application/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+using Portfolio.Domain.Common;
+
+namespace Portfolio.Application.Interfaces;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+    Task AddAsync(T entity, CancellationToken cancellationToken = default);
+    void Update(T entity);
+    void Delete(T entity);
+}
