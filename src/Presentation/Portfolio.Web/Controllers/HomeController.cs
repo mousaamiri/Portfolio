aae7eb40cd14c3facc2cbda0a851e0cccc857005
mousaamiri <106370749+mousaamiri@users.noreply.github.com>
2026-07-05 15:25:15 +0330

    feat: scaffold Portfolio.Web MVC project structure

---
+++ src/Presentation/Portfolio.Web/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Web.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
